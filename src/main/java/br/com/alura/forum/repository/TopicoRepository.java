33fe0b37ed3015ced9a48a6973ecc01a953c321c
Stephanie dos Santos Amaral <stephanie.amaral@zup.com.br>
2021-07-19 21:10:34 -0300

    Curso de Spring

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    //underline em caso de ambiguidade nos casos de nomes iguais em RELACIONAMENTO e tudo junto entre atributos.
+    //List<Topico> findByCurso_Nome(String nomeCurso);
+
+    List<Topico> findByCursoNome(String nomeCurso);
+
+
+    //Não segue o padrão
+    // @Query("SELECT t FROM topico t WHERE t.curso.nome = :nomeCurso")
+    //List<Topico> carregarPorNome(@Param("nomeCurso")String nomeCurso);
+}
